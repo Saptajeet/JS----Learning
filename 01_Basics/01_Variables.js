c0f092cabc76811f84cc0a700c7c2291b984b971
Saptajeet <157672329+Saptajeet@users.noreply.github.com>
2024-12-23 18:03:12 +0000

    Variables In JS

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,13 @@
+const accountId = 1224456
+let accountEmail = "aditya@google.com"
+var accountPassword = "54321"
+accountCity = "Mumbai"
+
+// accountId = 2 // Not Allowed
+console.log(accountId);
+
+accountEmail = "adi@hotmail.com"
+accountPassword = "12345"
+accountCity = "Delhi"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
