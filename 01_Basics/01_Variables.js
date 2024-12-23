cf9f0bcaa7a606d31131d50b1a63cc22eeaf416c
Saptajeet <157672329+Saptajeet@users.noreply.github.com>
2024-12-23 18:05:44 +0000

    Changes In Variables In JS

--- 01_Basics/01_Variables.js
+++ 01_Basics/01_Variables.js
@@ -2,6 +2,7 @@ const accountId = 1224456
 let accountEmail = "aditya@google.com"
 var accountPassword = "54321"
 accountCity = "Mumbai"
+let accountState
 
 // accountId = 2 // Not Allowed
 console.log(accountId);
@@ -10,4 +11,9 @@ accountEmail = "adi@hotmail.com"
 accountPassword = "12345"
 accountCity = "Delhi"
 
-console.table([accountId, accountEmail, accountPassword, accountCity])
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/*
+Prefer not to use var,
+because of issues in block scope and functional scope.
+*/
