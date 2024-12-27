8a54318e3e279a6c4c77eb69b2ac232d1e8113e4
Saptajeet <157672329+Saptajeet@users.noreply.github.com>
2024-12-27 09:48:51 +0000

    Changes In 02_Data_Types

--- 01_Basics/02_Data_Types.js
+++ 01_Basics/02_Data_Types.js
@@ -1,1 +1,11 @@
 "use strict"; // treat all JS code as newer version
+
+// alert (3+3) // we are using nodeJS not a browser
+
+console.log(3
+    +
+    3)
+
+console.log("Aditya")
+
+// code readability should be high
