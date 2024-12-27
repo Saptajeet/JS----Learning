ccb2ebfbbf24d4c26e5cfdb3c33bbc7e9828fda8
Saptajeet <157672329+Saptajeet@users.noreply.github.com>
2024-12-27 11:20:16 +0000

    Start of ConversionOps

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,65 @@
+let score1 = "33"
+
+// console.log(typeof score1);
+// console.log(typeof(score1));
+
+let valueInNumber1 = Number(score1)
+
+// console.log(typeof valueInNumber1);
+// console.log(valueInNumber1);
+
+
+let score2 = "33abc"
+
+// console.log(typeof score2);
+// console.log(typeof(score2));
+
+let valueInNumber2 = Number(score2)
+
+// console.log(typeof valueInNumber2);
+// console.log(valueInNumber2);
+
+
+let score3 = null
+
+// console.log(typeof score3);
+// console.log(typeof(score3));
+
+let valueInNumber3 = Number(score3)
+
+// console.log(typeof valueInNumber3);
+// console.log(valueInNumber3);
+
+
+let score4 = false
+
+// console.log(typeof score4);
+// console.log(typeof(score4));
+
+let valueInNumber4 = Number(score4)
+
+// console.log(typeof valueInNumber4);
+// console.log(valueInNumber4);
+
+let score5 = undefined
+
+// console.log(typeof score5);
+// console.log(typeof(score5));
+
+let valueInNumber5 = Number(score5)
+
+// console.log(typeof valueInNumber5);
+// console.log(valueInNumber5);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "abc"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 0 => false; 1 => true
+// "" => false; "abc..." => true
+// null => false; undefined => false
