9b9facff36d228b1df5c739bdca756af73e7137e
Saptajeet <157672329+Saptajeet@users.noreply.github.com>
2025-01-01 12:50:37 +0000

    Heap & Stack memory

--- 01_Basics/datatypes--summary.js
+++ 01_Basics/datatypes--summary.js
@@ -1,4 +1,4 @@
-// Primitive Datatypes
+// Primitive Datatypes (Passed by value, meaning a copy of the value is passed to the method)
 // 7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
 
 const score = 100
@@ -15,27 +15,50 @@ const anotherId = Symbol('123')
 
 const bigNumber = 927489234923289429n
 
-// ****************************************************************************************************************
+// ************************************************************************************************************************************************************************
 
-// Reference Type/ Non Primitive Datatypes
+// Reference Type/ Non Primitive Datatypes (Passed by reference, meaning a reference to the memory location of the variable is passed to the method)
 // Array, Objects, Functions
 
 const avengers = ["iron man", "captain america", "hulk", "hawkeye", "black widow", "thor"]; // array
 
 let myObj = {
     name : "Aditya",
-    age : 18,
+    age : 18
 } // object
 
 const myFunction = function name(){
     console.log("Hello World");        
 } // function
 
-console.log(typeof bigNumber); // to tell tyoe of datatype of a variable
-console.log(typeof myFunction); // => Function
-console.log(typeof myObj); // => Object
-console.log(typeof avengers); // => Object (Array)
+// console.log(typeof bigNumber); // to tell tyoe of datatype of a variable
+// console.log(typeof myFunction); // => Function
+// console.log(typeof myObj); // => Object
+// console.log(typeof avengers); // => Object (Array)
 
 //JavaScript is dynamically typed, which means that the type of a variable is assigned at runtime based on the value of the variable
 
 // Link to Study: https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// ************************************************************************************************************************************************************************
+
+// Stack Memory(Primitive), Heap memory(Non-Primitive)
+
+let myGameName = "JesusInGucci"
+
+let anotherName = myGameName
+anotherName = "JesusInDior"
+
+console.log(myGameName);
+console.log(anotherName);
+
+let userOne = {
+    email : "playerone@google.com",
+    upiId: "abc@ybl"
+}
+
+let userTwo = userOne;
+userTwo.email = "playertwo@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
